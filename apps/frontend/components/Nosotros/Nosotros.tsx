aeb299421605ab461e7297b49ee03142bf8c33b3
Alex Artavia <joseaaq5@gmail.com>
2026-06-28 10:25:14 -0600

    Update Nosotros.tsx

--- apps/frontend/components/Nosotros/Nosotros.tsx
+++ apps/frontend/components/Nosotros/Nosotros.tsx
@@ -2,7 +2,6 @@ import { Target, Eye, Heart, CheckCircle, User, Scale } from 'lucide-react'
 import Image from 'next/image'
 import styles from './Nosotros.module.css'
 import { NosotrosData } from '@/types/nosotros'
-import { Video } from '@/components/Video/Video'
 
 export function Nosotros({ data }: { data: NosotrosData }) {
   return (
@@ -108,11 +107,6 @@ export function Nosotros({ data }: { data: NosotrosData }) {
               </div>
             ))}
           </div>
-        </div>
-
-        {/* Video Destacado */}
-        <div style={{ marginTop: '3rem' }}>
-          <Video />
         </div>
       </div>
     </section>
